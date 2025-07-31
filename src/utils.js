d541710ec0294d701260af1a6bde888d85272b31
vladislav <vlad.strebkov.00@mail.ru>
2025-07-31 16:26:17 +0300

    chore(utils): удалена неиспользуемая функция

--- src/utils.js
+++ src/utils.js
@@ -198,25 +198,6 @@ const formatDateForOrder = (dateString) => {
 }
 
 
-const formatDateForPicker = (date) => {
-    if (date == null) {
-        return null;
-    } 
-    try {
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const year = date.getFullYear();
-        return `${day}.${month}.${year}`;
-    } catch {
-        const parsedDate = new Date(date);
-        const day = String(parsedDate.getDate()).padStart(2, '0');
-        const month = String(parsedDate.getMonth() + 1).padStart(2, '0');
-        const year = parsedDate.getFullYear();
-        return `${day}.${month}.${year}`;
-    }
-}
-
-
 const formatTime = (dateString) => {
     if (!dateString) return '';
     const date = new Date(dateString);
@@ -297,7 +278,6 @@ export {
     getOrderState,
     getOrderStateEn,
     formatDateForOrder,
-    formatDateForPicker,
     getFormatDate,
     formatTime,
     getReviewText,
