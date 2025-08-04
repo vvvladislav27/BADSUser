8c2f0d495e4fa6d7ca140f6330e71e1e57d51fd0
vladislav <vlad.strebkov.00@mail.ru>
2025-08-04 16:45:05 +0300

    fix(App): попытка обновлять состояние при закрытии

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,17 @@
 import { onBeforeMount, ref } from 'vue';
 import store from './store';
 
+const tg = window.Telegram.WebApp
 const isBlocked = ref()
 
 onBeforeMount(async() => {
   isBlocked.value = await store.dispatch("GET_AND_SET_DATA")
+  tg.onEvent("deactivated", eventHandler)
 })
+
+const eventHandler = () => {
+  store.dispatch("RESET_VIDEO_LOADED")
+}
 
 </script>
 
