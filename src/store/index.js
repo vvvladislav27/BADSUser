81afbe7fa3ebd2b3c9a5c4870e41811049518fab
vladislav <vlad.strebkov.00@mail.ru>
2025-08-04 16:48:00 +0300

    fix(store): мелкое исправление

--- src/store/index.js
+++ src/store/index.js
@@ -33,7 +33,6 @@ const state = {
     key: 'reload-data',
     storage: window.localStorage,
     reducer: (state) => ({
-      isVideoVisible: state.isVideoVisible,
       userCart: state.userCart,
       userCartItems: state.userCartItems,
       userCartFoodSups: state.userCartFoodSups,
