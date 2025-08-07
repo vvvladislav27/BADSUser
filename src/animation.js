22791e80b8e3dfa038fdc9384a28e3672bca19b4
vladislav <vlad.strebkov.00@mail.ru>
2025-08-07 12:53:34 +0300

    fix(anumation): исправлена ошибка в анимации текста

--- src/animation.js
+++ src/animation.js
@@ -12,21 +12,29 @@ const setAnimationForText = (customClass) => {
         const animationDuration = Math.max(5, (textWidth / 100) * 5);
         if (textOverflow <= 0) return;
         const uniqueClass = `animate-name-${item.id}`;
-        item.classList.add(uniqueClass);
-        item.parentElement.style.overflow = 'hidden';
-        cssRules += 
-            `.${uniqueClass} {
-                animation: float-animation-${item.id} ${animationDuration}s linear infinite;
-                white-space: nowrap;
-            }
-            @keyframes float-animation-${item.id} {
-                0% { transform: translateX(0); opacity: 1; }
-                25% { transform: translateX(0); opacity: 1; }
-                50% { transform: translateX(-${textOverflow}px); opacity: 1; }
-                75% { transform: translateX(-${textOverflow}px); opacity: 1; }
-                100% { transform: translateX(0); opacity: 1; }
-            }
-        `;
+        if (!item.classList.contains(uniqueClass)) {
+            item.classList.add(uniqueClass);
+            item.parentElement.style.overflow = 'hidden';
+            cssRules += 
+                `.${uniqueClass} {
+                    animation: float-animation-${item.id} ${animationDuration}s linear infinite;
+                    white-space: nowrap;
+                }
+                @keyframes float-animation-${item.id} {
+                    0% { transform: translateX(0); opacity: 1; }
+                    25% { transform: translateX(0); opacity: 1; }
+                    50% { transform: translateX(-${textOverflow}px); opacity: 1; }
+                    75% { transform: translateX(-${textOverflow}px); opacity: 1; }
+                    100% { transform: translateX(0); opacity: 1; }
+                }
+            `;
+        }
+    });
+    const existingStyles = document.querySelectorAll('style');
+    existingStyles.forEach(existingStyle => {
+        if (existingStyle.innerText.includes("float-animation")) {
+            existingStyle.remove();
+        }
     });
     style.innerText = cssRules;
     document.head.appendChild(style);
