4c9f66a071edef770add5f53e3f69dc59bebcb06
vladislav <vlad.strebkov.00@mail.ru>
2025-08-12 11:57:05 +0300

    fix(MOrder): удалено лишнее кстановка кнопки назад

--- src/components/MOrder.vue
+++ src/components/MOrder.vue
@@ -67,9 +67,6 @@ onBeforeMount(async() => {
         router.push("/second-app/orders");
     };
     backButton.onClick(backButtonClickHandler);
-    if (!backButton.isVisible) {
-        backButton.show();
-    }
     for (let item of order.value.items) {
         await getImage(item.food_sup.photo_path)
     }
