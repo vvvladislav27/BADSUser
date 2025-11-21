8b7b2c5e71f5216f7b625b8437f580a69626fa1d
vladislav <vlad.strebkov.00@mail.ru>
2025-11-21 10:30:37 +0300

    fix(MOrderFilter): исправлен список со статусами заказов согласно ТЗ

--- src/components/MOrderFilter.vue
+++ src/components/MOrderFilter.vue
@@ -10,7 +10,7 @@ import { mainButton, secondaryButton, hideButton, setupButton, backButton } from
 const format = "dd.MM.yyyy"
 
 const filterList = ref([]);
-const buttons = ["all", "created", "packed", "send", "received", "finished", "canceled", "arrived", "paid"]
+const buttons = ["all", "created", "canceled", "processed"]
 const filters = computed(() => store.state.orderFilters);
 
 const isContextMenuStateVisible = ref(false);
